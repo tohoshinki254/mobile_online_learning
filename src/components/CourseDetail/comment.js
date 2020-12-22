27b9816f35fcfe94e9f6978d6e6efa8917c59d76
Thanh Tien <tiennt499@gmail.com>
2020-12-22 20:57:59 +0700

    + Thêm thống kê đánh giá khóa học

--- src/components/CourseDetail/comment.js
+++ src/components/CourseDetail/comment.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ProgressBar, Colors } from 'react-native-paper';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import Rating from '../Common/rating';
 
@@ -19,7 +18,22 @@ const renderComment = (item) => (
     </View>
 )
 
+const percentBar = (percent) => (
+    <View style={{ borderRadius: 50, backgroundColor: '#616161', width: '70%', marginLeft: 10, height: 10}}>
+        <Animated.View style={[StyleSheet.absoluteFill, { borderRadius: 50, backgroundColor: '#fbc02d', width: `${percent}%` }]}/>
+    </View>
+)
+
 const Comment = ({ details }) => {
+    const star = [0, 0, 0, 0, 0];
+    for (let i = 0; i < details.ratings.ratingList.length; i++) {
+        star[Math.floor(details.ratings.ratingList[i].averagePoint) - 1]++;
+    }
+
+    for (let i = 0; i < 5; i++) {
+        star[i] = star[i] * 20;
+    }
+
     return (
         <View style={{ marginLeft: 10, marginRight: 10, marginBottom: 10}}>
             <View style={{ flexDirection: 'row', marginBottom: 20 }}>
@@ -31,27 +45,27 @@ const Comment = ({ details }) => {
                     <View style={styles.percent}>
                         <Text style={{ color: 'grey' }}>5&nbsp;</Text>
                         <FontAwesome name="star" color="#fbc02d" size={15} />
-                        <ProgressBar progress={0.5} color={Colors.red800} />
+                        {percentBar(star[4])}
                     </View>
                     <View style={styles.percent}>
                         <Text style={{ color: 'grey' }}>4&nbsp;</Text>
                         <FontAwesome name="star" color="#fbc02d" size={15} />
-                        <ProgressBar progress={0.5} color="#fbc02d"/>
+                        {percentBar(star[3])}
                     </View>
                     <View style={styles.percent}>
                         <Text style={{ color: 'grey' }}>3&nbsp;</Text>
                         <FontAwesome name="star" color="#fbc02d" size={15} />
-                        <ProgressBar progress={0.5} color="#fbc02d"/>
+                        {percentBar(star[2])}
                     </View>
                     <View style={styles.percent}>
                         <Text style={{ color: 'grey' }}>2&nbsp;</Text>
                         <FontAwesome name="star" color="#fbc02d" size={15} />
-                        <ProgressBar progress={0.5} color="#fbc02d"/>
+                        {percentBar(star[1])}
                     </View>
                     <View style={styles.percent}>
                         <Text style={{ color: 'grey' }}>1&nbsp;</Text>
                         <FontAwesome name="star" color="#fbc02d" size={15} />
-                        <ProgressBar progress={0.5} color="#fbc02d"/>
+                        {percentBar(star[0])}
                     </View>
                 </View>
             </View>
@@ -67,7 +81,8 @@ export default Comment;
 const styles = StyleSheet.create({
     percent: {
         flexDirection: 'row',
-        margin: 5
+        margin: 5,
+        alignItems: 'center'
     },
     averagePoint: {
         fontSize: 35,
